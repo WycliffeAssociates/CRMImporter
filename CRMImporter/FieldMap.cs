f6ce8bd288991574f81922128f55176396916b71
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2018-03-28 15:25:35 -0400

    Fix an issue, add nuspec, and more comments (#3)

--- CRMImporter/FieldMap.cs
+++ CRMImporter/FieldMap.cs
@@ -12,6 +12,12 @@ namespace CRMImporter
         public string TargetField;
         public IConverter Convert;
 
+        /// <summary>
+        /// FieldMap constructor
+        /// </summary>
+        /// <param name="source">The field in your source data to match against</param>
+        /// <param name="target">The field in CRM to match to</param>
+        /// <param name="converter">Optional conversion function</param>
         public FieldMap(string source, string target, IConverter converter = null)
         {
             this.SourceField = source;
