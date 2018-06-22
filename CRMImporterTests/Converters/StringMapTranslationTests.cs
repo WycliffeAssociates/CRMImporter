9bf2e6146233227d4ae2d2ac52efa2b2d29c2a5f
Reuben Swartz <reuben_swartz@wycliffeassociates.org>
2018-06-22 13:37:12 -0400

    More tests

---
+++ CRMImporterTests/Converters/StringMapTranslationTests.cs
@@ -0,0 +1,31 @@
+﻿using CRMImporter.Converters;
+using FakeXrmEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xrm.Sdk;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMImporterTests.Converters
+{
+    [TestClass]
+    public class StringMapTranslationTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            XrmFakedContext context = new XrmFakedContext();
+            IOrganizationService service = context.GetOrganizationService();
+            string original = "Original";
+            string converted = "New";
+            StringMapTranslation converter = new StringMapTranslation(new Dictionary<string, string>
+            {
+                [original] = converted
+            });
+            Assert.AreEqual(converted, converter.Convert(original, service));
+            Assert.AreEqual(null, converter.Convert("UNKNOWN", service));
+        }
+    }
+}
